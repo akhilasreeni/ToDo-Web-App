7a21524f6e52aad05a4dc9c8a01fc76840cb636a
AKHILA S <48093560+akhilasreeni@users.noreply.github.com>
2022-04-08 23:35:48 +0530

    initial commit

--- script1.js
+++ script1.js
@@ -1,36 +1,28 @@
-let uname =document.getElementById("username");
-let pwd =document.getElementById("password");
-let unameErr=document.getElementById("nameErr");
-let pwdErr=document.getElementById("passErr");
+var username=document.getElementById("username");
+var password=document.getElementById("password");
+var unameErr=document.getElementById("nameErr");
+var pwdErr=document.getElementById("passErr");
+var form=document.getElementById("form1");
 function validateForm(callback){
-    var regexp = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9\-]+)(\.)([a-z]{2,3})(\.[a-z]{2,3})?$/;
-    if(uname.value=="admin" && pwd.value=="12345"){
-        callback(true);
+    if(username.value=="admin" && password.value=="12345"){
+        callback();
     }
     else{
-        if(regexp.test(uname.value)){
-            error.innerHTML=" Invalid Username";
-           
-    }
-    else{
-        if(email.value==""){
-            
-            error.innerHTML=" ";
+        if(username.value!="admin"){
+            unameErr.innerHTML="Invalid username";
+            return false;
         }
-        else{ 
-        error.innerHTML="Invalid Email-id";
-        error.style.color="red";
-        return false;
-        }
-       
-    }
-*/
-    function callback(val){
-        if(val=='true'){
-            location.href="./main.html";
-        }
-        else{
-            pwdErr.innerHTML="Invalid Email-id";
+        if(password.value!="12345"){
+            pwdErr.innerHTML="Invalid password";
+            return false;
         }
     }
 }
+function callback(){
+    form.setAttribute("action","main.html");
+    return true;
+}
+
+
+
+
